70706fdd8c421e57478b7a6ab0f70243615e9c76
Kevin Tran <kevin2005tran@gmail.com>
2016-11-06 13:02:22 -0800

    super sick this weekend, slow start on project... created all main docs and linked necessary libraries

---
+++ main.js
@@ -0,0 +1,23 @@
+// js check
+// alert('test');
+// jquery check
+// $( document ).ready(function(){
+//   $('h1').css('background-color', 'orange');
+// });
+
+$( document ).ready(function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
