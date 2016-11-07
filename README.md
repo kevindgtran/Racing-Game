53e69241687ba81c28db6cfbdd9e56ee3095ae62
Kevin Tran <kevin2005tran@gmail.com>
2016-11-06 20:18:50 -0800

    successfully added game winner alert and updated README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Racing-Game
-Project 0 - Racing Game
+
+Technologies used: HTML, CSS, Javascript, jQuery, Bootstrap, Google Fonts, Animate CSS and Photoshop
+
+Existing features: Googled, cropped and edited all images. Leveraged youtube tutorials and jQuery documentation.
+
+Planned features: Would like to add sounds, count down and games won tracker!
+
+Addition: This was a fun project with lots of learnings. I went back and read the previous lecture notes and did a lot of the documentation examples. Overall I am happy with the outcome of the game and will add more to it later!
--- main.js
+++ main.js
@@ -6,49 +6,42 @@
 // });
 
 $( document ).ready(function(){
+//putting elements into variables
 var player1 = '<div id="player1"><img src="images/marioCart.png" alt="Kevin" /></div>';
 var player2 = '<div id="player2"><img src="images/sonicCar2.png" alt="sonic car" /></div>';
 var background = '<div id="background"><img src="images/MarioBackGround.jpg" alt="MarioBackGround" /></div>'
 var background2 = '<div id="background"><img src="images/SonicBackGround.png" alt="SonicBackGround" /></div>'
+var player1Win = 0;
+var player2Win = 0;
 
+//appending player and background images
 $('.gameBoxTop').append(player1);
 $('.gameBoxTop').append(background);
 $('.gameBoxBottom').append(player2);
 $('.gameBoxBottom').append(background2);
 
-
-$( document ).keydown(function(e) {
-  // alert(e.keyCode);
-  //Q keyCode = 81
-  //} keyCode = 221
-var position = $('#player1').position();
-var position2 = $('#player2').position();
-
-if (e.keyCode === 81) {
-  $('#player1 img').css('left', '+=10px');
-}
-
-if (e.keyCode === 221) {
-  $('#player2 img').css('left', '+=10px');
-}
-
-
-
-
+//reset button to reload page
+$('.reset').click(function(){
+  location.reload();
 });
 
+//user keydown actions
+$( document ).keydown(function(e) {
+      if (e.keyCode === 81) {
+        $('#player1 img').css('left', '+=10px');
+        player1Win += 1;
+        if (player1Win > 103) {
+          alert('Player 1 Wins!')
+        }
+      }
 
+      if (e.keyCode === 221) {
+        $('#player2 img').css('left', '+=10px');
+        player2Win += 1;
+        if (player2Win > 103) {
+          alert('Player 2 Wins!')
+        }
+      }
 
-
-
-
-
-
-
-
-
-
-
-
-
+  });
 });
